9bfc389fda7309cb1c745ced25eed0da12deba9b
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-18 14:35:12 +0300

    find, findIndex and every added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const result = document.querySelector("#cikti");
 
-
 let diziNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let diziStr = ["okan", "nurdoğan", "enes", "alper"];
 
+let dizi = [];
+//! .find() 4 sayısından küçük olan ilk elemanı döner findLast bunun tam tersini yapar.
+// const dizim = diziNum.find((x) => x < 4);
+// console.log(dizim);
+// result.textContent = dizim;
 
-//! .find()
+//! .finIndex() 4 sayısından büyük olan ilk elemanın index numarasını döndürür
+// const dizim = diziNum.findIndex((x) => x > 4);
+// console.log(dizim);
+// result.textContent = dizim;
+
+//! .evert() dizi içerisindeki tüm elemanları koşula tabi tutar eğer hepsi koşula uyuyorsa true yoksa false döner.
+// const dizim = diziStr.every((x) => x === "okan");
+// console.log(dizim);
+// result.textContent = dizim;
