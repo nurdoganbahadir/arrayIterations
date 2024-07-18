1bf83301aea285ad2fa7971f1d373c7e97b545b8
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-18 14:43:53 +0300

    some and forEach added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const result = document.querySelector("#cikti");
 
 let diziNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let diziStr = ["okan", "nurdoğan", "enes", "alper"];
+let diziStr = ["okan", "   nurdoğan", "enes", "alper"];
 
 let dizi = [];
 //! .find() 4 sayısından küçük olan ilk elemanı döner findLast bunun tam tersini yapar.
@@ -18,3 +18,12 @@ let dizi = [];
 // const dizim = diziStr.every((x) => x === "okan");
 // console.log(dizim);
 // result.textContent = dizim;
+
+//! .some() dizi içerisindeki tek bir eleman bile koşula uyuyorsa true döndürür.
+// const dizim = diziStr.some((x) => x === "okan");
+// console.log(dizim);
+// result.textContent = dizim;
+
+//! .forEach dizinin içerisindeki indeksleri döner ve döndürür yeni dizi oluşturmaz
+const dizim = diziStr.forEach((x) => console.log(x.toLocaleUpperCase().trim()));
+
